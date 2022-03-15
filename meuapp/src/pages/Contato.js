fbde01b5f192f22be5f07eb37d5573f46c76cf29
Pedro Arns Junior <parnsjr@gmail.com>
2022-03-15 10:49:28 -0300

    delete

--- meuapp/src/pages/Contato.js
+++
@@ -1,9 +0,0 @@
-function Contato(){
-    return(
-       <div>
-           <h1>Contato</h1>
-           <p>conteudo de contatos</p>
-       </div>
-    );
-}
-export default Contato;
--- meuapp/src/pages/Empresa.js
+++
@@ -1,9 +0,0 @@
-function Empresa(){
-    return(
-       <div>
-           <h1>Empresa</h1>
-           <p>bem vindo a pagina da empresa</p>
-       </div>
-    );
-}
-export default Empresa;
