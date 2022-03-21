443e415312aaed38211eba5f42a5abb3d8ce9ce4
Pedro Arns Junior <parnsjr@gmail.com>
2022-03-21 18:37:27 -0300

    Footer, Navbar

--- costs/src/components/AppIndex.js
+++ costs/src/components/AppIndex.js
@@ -1,30 +1,29 @@
 import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 import Company from "./pages/Company";
 import Contact from "./pages/Contact";
 import NewProject from "./pages/NewProject";
 
 import Container from "./layout/Container";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function AppIndex(){
    return(
        <Router>
-          <div>
-              <Link to="/">Home</Link>
-              <Link to="/company">Empresa</Link>
-              <Link to="/contact">Contato</Link>
-              <Link to="/newproject">Novo Projeto</Link>
-          </div>
+          <Navbar/>
           
           <Container customClass="minHeight">  
             <Routes>
               <Route path="/" element={<Home/>}/>
+              <Route path="/projects" element={<Projects/>}/>
               <Route path="/company" element={<Company/>}/>
               <Route path="/contact" element={<Contact/>}/>
               <Route path="/newproject" element={<NewProject/>}/>
             </Routes>
           </Container>
-          <p>Footer</p> 
+          <Footer/>
        </Router>
        
    );
